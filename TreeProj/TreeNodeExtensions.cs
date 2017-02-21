9e6ba0ac0d341c7b0277ffcf51c7e3f1f4d174f7
martinkack <martinkack@hotmail.com>
2017-02-21 14:06:52 +0100

    First commit

---
+++ TreeProj/TreeNodeExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeProj
+{
+    public static class TreeNodeExtensions
+    {
+        /// <summary>
+        /// Creates an iterator that traverses the tree breadth first.
+        /// </summary>
+        /// <param name="taxonTreeNodeList">The Taxon tree node list.</param>        
+        public static IEnumerable<TreeNode> AsBreadthFirstIterator(this TreeNodeList taxonTreeNodeList)
+        {
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            foreach (TreeNode taxonTreeNode in taxonTreeNodeList)
+            {
+                q.Enqueue(taxonTreeNode);
+            }
+
+            while (q.Any())
+            {
+                TreeNode current = q.Dequeue();
+                if (current != null)
+                {
+                    if (current.Children != null)
+                    {
+                        foreach (TreeNode childNode in current.Children)
+                        {
+                            q.Enqueue(childNode);
+                        }
+                    }
+
+                    yield return current;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates an iterator that traverses the tree depth first.
+        /// </summary>
+        /// <param name="taxonTreeNodeList">The Taxon tree node list.</param>        
+        public static IEnumerable<TreeNode> AsDepthFirstIterator(this TreeNodeList taxonTreeNodeList)
+        {
+            Stack<TreeNode> q = new Stack<TreeNode>();
+            foreach (TreeNode taxonTreeNode in taxonTreeNodeList)
+            {
+                q.Push(taxonTreeNode);
+            }
+
+            while (q.Any())
+            {
+                TreeNode current = q.Pop();
+                if (current != null)
+                {
+                    if (current.Children != null)
+                    {
+                        for (int i = current.Children.Count - 1; i >= 0; i--)
+                        {
+                            q.Push(current.Children[i]);
+                        }
+                    }
+
+                    yield return current;
+                }
+            }
+        }
+    }
+}
---
+++ TreeProj/TreeNodeList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeProj
+{
+    public class TreeNodeList : List<TreeNode>
+    {
+        public TreeNodeList()
+        {
+            
+        }
+
+        public TreeNodeList(TreeNode treeNode)
+        {
+            Add(treeNode);
+        }
+
+        public TreeNodeList(params TreeNode[] treeNodes)
+        {
+            AddRange(treeNodes);
+        }
+    }
+}
